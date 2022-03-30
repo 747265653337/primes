b2ebfaff4dd9959c44dfd79fd8d9d231427765af
747265653337 <0x25mb@protonmail.com>
2022-03-30 16:15:27 +0200

    Update compact_primes.py

---
+++ compact_primes.py
@@ -0,0 +1,2 @@
+prime_list = [number for number in range(2, int(input('amount:'))) if not any([(number % factor == 0)for factor in (prime_list if 'prime_list' in locals() else [2])])]
+print(prime_list)
