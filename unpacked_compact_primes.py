e85e9dfb0224daf081604deb51bd021b7a72e114
747265653337 <0x25mb@protonmail.com>
2022-03-30 21:56:30 +0200

    unpacked algorithm for potential improvements

---
+++ unpacked_compact_primes.py
@@ -0,0 +1,18 @@
+import time
+# * print([2]+[number for number in range(3, int(input('amount:')), 2) if not any([(number % factor == 0) for factor in range(3,round(number**0.5), 2)])])
+
+# writing the code out
+prime_list = [2]
+amount = int(input('amount:'))+1
+start = time.perf_counter()
+for number in range(3, amount, 2):
+    for factor in range(3, round(number**0.5)+1, 2):
+        if number % factor == 0:
+            break
+    else:
+        prime_list.append(number)
+    
+end = time.perf_counter() - start
+
+print(len(prime_list))
+print(f'{end}seconds')
