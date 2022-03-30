daa274b11a4fd3b756f671be396a900b0e70d20d
747265653337 <0x25mb@protonmail.com>
2022-03-30 22:05:49 +0200

    Update fastest_primes.py

--- fastest_primes.py
+++ fastest_primes.py
@@ -1,1 +1,24 @@
-# not yet...
+# fastest algorithm i made so far
+
+from math import sqrt
+import time
+
+amount = 100000
+prime_list = [2,3]
+number = 3
+start_time = time.perf_counter()
+for primenumber in range(2, amount):
+    prime_found = False
+    while not prime_found:
+        number += 2
+        lim = sqrt(number)
+        checking = True
+        pointer = 0
+        while checking:
+            check = prime_list[pointer]
+            pointer += 1
+            prime_found = not (number % check == 0)
+            checking = check <= lim and prime_found
+    prime_list.append(number)
+end_time = time.perf_counter() - start_time
+print(f'{end_time} seconds')
