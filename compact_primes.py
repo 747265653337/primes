f90c051a12df8178c0f6f4fa90aef2c569dec00a
747265653337 <0x25mb@protonmail.com>
2022-03-30 16:31:51 +0200

    Update compact_primes.py

--- compact_primes.py
+++ compact_primes.py
@@ -1,4 +1,5 @@
-
+import time
 # * slowest algorithm ever written
-print([2]+[number for number in range(3, int(input('amount:')), 2) if not any([(number % factor == 0) for factor in range(3,round(number**0.5), 2)])])
-
+start = time.perf_counter()
+[2]+[number for number in range(3, 1299710, 2) if not any([(number % factor == 0) for factor in range(3,round(number**0.5), 2)])]
+print(f'{time.perf_counter() - start} seconds')
