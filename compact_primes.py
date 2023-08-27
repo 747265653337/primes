0ced25fa029120f2876a1b2d645baa37dab7013c
747265653337 <0x25mb@protonmail.com>
2023-08-27 18:07:38 +0200

    loose bugreport for compact_primes

--- compact_primes.py
+++ compact_primes.py
@@ -1,3 +1,4 @@
 
 # * slowest algorithm ever written
 print([2]+[number for number in range(3, int(input('amount:'))+1, 2) if not any([(number % factor == 0) for factor in range(3,round(number**0.5+1), 2)])])
+# breaks for too high amounts, by not printing the full string
